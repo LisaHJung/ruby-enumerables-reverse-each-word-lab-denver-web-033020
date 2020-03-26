0273dd28bdc8515d59e8e0f24921bc990b107767
Lisa Jung <lisahjung@gmail.com>
2020-03-26 23:09:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,33 +1,3 @@
-require "pry"
-def reverse_each_word (string)
-  array_string = string.split(" ")
-  new_array =[]
-  new_array = array_string.collect{|word| word.reverse}
-  new_array.join(" ")
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
-def reverse_each_word (string)
-  array_string= string.split(" ")
-  new_array = []
-  new_array = array_string.collect{|word| word.reverse}
-=======
 def reverse_each_word(sentence)
   words_array = sentence.split(" ")
   new_array = []
