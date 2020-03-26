bfef1375fefa9defad094475adc584555f5f593b
Lisa Jung <lisahjung@gmail.com>
2020-03-26 23:03:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
+require "pry"
 def reverse_each_word (string)
   array_string = string.split(" ")
   new_array =[]
   new_array = array_string.each { |word| word.reverse}
+  binding.pry
   new_array.join(" ")
+  
 end
 
 
