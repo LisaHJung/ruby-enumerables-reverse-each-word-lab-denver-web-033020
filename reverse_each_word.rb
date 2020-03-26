5b89ada1907e0e292bddf714be4eb84394d9023a
Lisa Jung <lisahjung@gmail.com>
2020-03-26 22:56:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+=begin
 def reverse_each_word (string)
   array_string= string.split(" ")
   new_array = []
@@ -8,8 +8,8 @@ def reverse_each_word(sentence)
   words_array = sentence.split(" ")
   new_array = []
   new_array = words_array.collect {|word| word.reverse }
->>>>>>> b4f137abc3b1a4e39b5113d0a0d8a8f884ac02e2
+
   new_array.join(" ")
 end
-
+=end
 
