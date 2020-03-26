75b53d4987d4b5a68139c62f44cbc79a446535fc
Lisa Jung <lisahjung@gmail.com>
2020-03-26 23:07:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require "pry"
 def reverse_each_word (string)
   array_string = string.split(" ")
   new_array =[]
-  new_array = array_string.each { |word| word.reverse}
+  new_array = array_string.collect {|word| word.reverse}
   new_array.join(" ")
   binding.pry
 end
