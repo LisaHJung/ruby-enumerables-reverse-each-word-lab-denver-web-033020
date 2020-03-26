dce1947ac598ce56460b1350c00280ecc0a959c6
Lisa Jung <lisahjung@gmail.com>
2020-03-26 23:03:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,6 @@ def reverse_each_word (string)
   array_string = string.split(" ")
   new_array =[]
   new_array = array_string.each { |word| word.reverse}
-  binding.pry
   new_array.join(" ")
   
 end
